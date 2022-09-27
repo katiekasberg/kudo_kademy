7940c243e9539d75b485c0040e0c475086013c59
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2022-09-26 21:57:25 -0500

    Task: Build out Model Classes

---
+++ capstone/java/src/main/java/com/techelevator/model/school/school.java
@@ -0,0 +1,50 @@
+package com.techelevator.model.school;
+
+public class school {
+    private int id;
+    private String name;
+    private String primaryColor;
+    private String secondaryColor;
+    private String logo;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrimaryColor() {
+        return primaryColor;
+    }
+
+    public void setPrimaryColor(String primaryColor) {
+        this.primaryColor = primaryColor;
+    }
+
+    public String getSecondaryColor() {
+        return secondaryColor;
+    }
+
+    public void setSecondaryColor(String secondaryColor) {
+        this.secondaryColor = secondaryColor;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+}
