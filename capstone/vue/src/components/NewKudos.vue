b0a7d473a85e151f410540b0ef337580cf1f66db
Kathryn Kasberg <katiekasberg@gmail.com>
2022-09-28 13:12:27 -0400

    working send kudos form

---
+++ capstone/vue/src/components/NewKudos.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <h1>SCHOOL COMMUNICATION</h1>
+    <img class="logo" src="../../resources/school-icon-9.png" alt="" />
+
+    <h2 class="h3 mb-3 font-weight-normal">Send A Kudo!</h2>
+
+    <form v-on:submit.prevent action="submit">
+      <label for="StudentId">Student Id:</label><br />
+      <input
+        v-model="kudo.studentId"
+        type="text"
+        id="StudentId"
+        name="StudentId"
+      /><br />
+      <input
+        v-model="kudo.typeId"
+        value="1"
+        type="radio"
+        id="KudosTypeA"
+        name="KudosType"
+      />
+      <label for="KudosTypeA">Kudos Type A</label><br />
+      <input type="radio" id="KudosTypeB" name="KudosType" />
+      <label for="KudosTypeB">Kudos Type B</label><br />
+      <input type="radio" id="KudosTypeC" name="KudosType" />
+      <label for="KudosTypeC">Kudos Type C</label><br />
+      <input type="radio" id="KudosTypeD" name="KudosType" />
+      <label for="KudosTypeD">Kudos Type D</label><br />
+      <label for="Comments">Comments:</label><br />
+      <input
+        v-model="kudo.message"
+        type="text"
+        id="Comments"
+        name="Comments"
+      /><br />
+      <button type="submit" v-on:click="saveKudo()">Send</button>
+    </form>
+  </div>
+</template>
+
+  <script>
+import kudoService from "../services/KudosService";
+export default {
+  data() {
+    return {
+      kudo: {
+        studentId: "",
+        typeId: "",
+        message: "",
+      },
+    };
+  },
+  methods: {
+    saveKudo() {
+      kudoService.SendKudos(this.kudo).then((response) => {
+        if (response.status === 200) {
+          //this.$router.push("/SendKudos");
+          this.kudo.studentId = "";
+          this.kudo.typeId = false;
+          this.kudo.message = "";
+        }
+      });
+    },
+  },
+};
+</script>
+    <style scoped>
+.send-kudo {
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  grid-gap: 7px;
+}
+img.logo {
+  margin: auto;
+  display: block;
+  width: 200px;
+  height: auto;
+}
+h1 {
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
+  color: darkorange;
+}
+h2 {
+  display: grid;
+  grid-area: left;
+  align-items: center;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
+form {
+  display: grid;
+  grid-area: auto;
+  align-items: center;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 10px;
+}
+label {
+  float: left;
+  clear: none;
+  display: block;
+  padding: 0px 0em 0px 20px;
+}
+
+input[type="radio"],
+input.radio {
+  float: left;
+  clear: none;
+  margin: 0px 200px 0px 0px;
+}
+</style>
+  
