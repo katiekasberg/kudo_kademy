729cd0d1bb8454bc1d626b1f2961b84c0e970b83
Laura Diaco <laura.a.diaco@gmail.com>
2022-09-28 17:58:22 -0400

    Added updateKudoType and addStudentToAClass to server

--- capstone/java/src/main/java/com/techelevator/dao/JdbcTeacherDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/JdbcTeacherDao.java
@@ -1,7 +1,27 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.kudo.Kudo;
+import com.techelevator.model.kudo.KudoRequest;
+import com.techelevator.model.school.ClassInfoStudent;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JdbcTeacherDao implements TeacherDao{
+
+    private JdbcTemplate jdbcTemplate;
+    public JdbcTeacherDao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void addStudentToAClass(ClassInfoStudent classInfoStudent) {
+        String sql = "INSERT INTO class_info_student(class_id, student_id) VALUES(?, ?);";
+        try {
+            jdbcTemplate.update(sql, classInfoStudent.getClassId(), classInfoStudent.getStudentId());
+        } catch (DataAccessException e) {
+            System.out.println(" Database access exception");
+        }
+    }
 }
