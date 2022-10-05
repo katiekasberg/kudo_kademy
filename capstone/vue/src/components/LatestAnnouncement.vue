0d8c243568bb15b8b610b3e8e0892b0f82cd1ff3
Kathryn Kasberg <katiekasberg@gmail.com>
2022-10-05 11:59:03 -0400

    login page

--- capstone/vue/src/components/LatestAnnouncement.vue
+++ capstone/vue/src/components/LatestAnnouncement.vue
@@ -34,7 +34,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .display-announce {
   display: grid;
   align-items: center;
--- capstone/vue/src/components/NewClass.vue
+++ capstone/vue/src/components/NewClass.vue
@@ -1,86 +1,84 @@
 <template>
   <div>
-      <form v-on:submit.prevent action="submit" class="newClassForm"></form>
-        <p>Creat a new Class</p>
-        <label for="ClassName">
-            Class Name:
-            <input type="text" v-model="newClass.name"/>
-        </label>
-        <label for="Subject">
-            Subject:
-            <input type="text" v-model="newClass.subject"/>
-        </label>
-        <label for="teacherId">
-            Teacher:
-            <input type="number" v-model="newClass.teacherId"/>
-        </label>
-        <label for="schoolId">
-            School:
-            <input type="number" v-model="newClass.schoolId"/>
-        </label>
-                <label for="description">
-            Description:
-            <input type="text" v-model="newClass.description"/>
-        </label>
-        <label for="period">
-            Period:
-            <input type="text" v-model="newClass.period"/>
-        </label>
-        <label for="startTime">
-            Start Time:
-            <input type="time" step= 2 v-model="newClass.startTime"/>
-        </label>        
-        <label for="endTime">
-            End Time:
-            <input type="time" step = 2 v-model="newClass.endTime"/>
-        </label>
-        <label for="schoolYear">
-            School Year:
-            <input type="number" value=2022 v-model="newClass.schoolYear"/>
-        </label>
-        <button type="submit" v-on:click="saveClass()">Submit</button>
-        
+    <form v-on:submit.prevent action="submit" class="newClassForm"></form>
+    <p>Creat a new Class</p>
+    <label for="ClassName">
+      Class Name:
+      <input type="text" v-model="newClass.name" />
+    </label>
+    <label for="Subject">
+      Subject:
+      <input type="text" v-model="newClass.subject" />
+    </label>
+    <label for="teacherId">
+      Teacher:
+      <input type="number" v-model="newClass.teacherId" />
+    </label>
+    <label for="schoolId">
+      School:
+      <input type="number" v-model="newClass.schoolId" />
+    </label>
+    <label for="description">
+      Description:
+      <input type="text" v-model="newClass.description" />
+    </label>
+    <label for="period">
+      Period:
+      <input type="text" v-model="newClass.period" />
+    </label>
+    <label for="startTime">
+      Start Time:
+      <input type="time" step="2" v-model="newClass.startTime" />
+    </label>
+    <label for="endTime">
+      End Time:
+      <input type="time" step="2" v-model="newClass.endTime" />
+    </label>
+    <label for="schoolYear">
+      School Year:
+      <input type="number" value="2022" v-model="newClass.schoolYear" />
+    </label>
+    <button type="submit" v-on:click="saveClass()">Submit</button>
   </div>
 </template>
 
 <script>
-import TeacherService from '../services/TeacherService';
+import TeacherService from "../services/TeacherService";
 export default {
-    data() {
-        return {
-            newClass: {
-                name: '',
-                subject: '',
-                teacherId: this.$store.state.user.id,
-                schoolId: 0,
-                description: '',
-                period:'',
-                startTime: '',
-                endTime: '',
-                schoolYear: 2022
-            },
-        };
-    },
-    methods: {
-        saveClass() {
-            TeacherService.addNewClass(this.newClass).then((response) => {
-                if(response.status === 201) {
-                    this.newClass.name = "";
-                    this.newClass.subject = "";
-                    this.newClass.teacherId = 0;
-                    this.newClass.schoolId = 0;
-                    this.newClass.description = "";
-                    this.newClass.period = "";
-                    this.newClass.startTime = "";
-                    this.newClass.endTime = "";
-                    this.newClass.schoolYear = 2022;
-                }
-            })
+  data() {
+    return {
+      newClass: {
+        name: "",
+        subject: "",
+        teacherId: this.$store.state.user.id,
+        schoolId: 0,
+        description: "",
+        period: "",
+        startTime: "",
+        endTime: "",
+        schoolYear: 2022,
+      },
+    };
+  },
+  methods: {
+    saveClass() {
+      TeacherService.addNewClass(this.newClass).then((response) => {
+        if (response.status === 201) {
+          this.newClass.name = "";
+          this.newClass.subject = "";
+          this.newClass.teacherId = 0;
+          this.newClass.schoolId = 0;
+          this.newClass.description = "";
+          this.newClass.period = "";
+          this.newClass.startTime = "";
+          this.newClass.endTime = "";
+          this.newClass.schoolYear = 2022;
         }
-    }
-}
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
