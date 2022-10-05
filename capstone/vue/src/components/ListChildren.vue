fe8add3d594035230450ea81635fcd6820f94582
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2022-10-05 11:41:32 -0500

    added image tag inside children table for parent

--- capstone/vue/src/components/ListChildren.vue
+++ capstone/vue/src/components/ListChildren.vue
@@ -1,57 +1,60 @@
 <template>
   <div>
-      <p>Children Assigned to this account:</p>
-      <table>
-          <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Image</th>
-          </tr>
-          <tr 
-            v-for="child in this.students"
-            v-bind:key="child.id"
-            v-bind:value="child.id"
-            >
-              <td>
-                  <router-link v-bind:to="{ name: 'StudentProfile', params: {id: child.id} }">
-                  {{child.firstName}} {{child.lastName}}
-                  </router-link>
-              </td>
-              <td>{{child.email}}</td>
-              <td>
-                   {{child.image}}
-              </td>
-          </tr>
-      </table>
+    <p>Children Assigned to this account:</p>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Image</th>
+      </tr>
+      <tr
+        v-for="child in this.students"
+        v-bind:key="child.id"
+        v-bind:value="child.id"
+      >
+        <td>
+          <router-link
+            v-bind:to="{ name: 'StudentProfile', params: { id: child.id } }"
+          >
+            {{ child.firstName }} {{ child.lastName }}
+          </router-link>
+        </td>
+        <td>
+          {{ child.email }}
+        </td>
+        <td>
+          <img v-bind:src='child.image' height=100 width=100 />
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import ParentService from "../services/ParentService"
+import ParentService from "../services/ParentService";
 
 export default {
-    name: 'ListChildren',
-    props: {
-        parentId: Number,
+  name: "ListChildren",
+  props: {
+    parentId: Number,
+  },
+  data() {
+    return {
+      students: [],
+    };
+  },
+  methods: {
+    getChildren() {
+      ParentService.getStudentsByParentId(this.parentId).then((response) => {
+        this.students = response.data;
+      });
     },
-    data() {
-        return {
-            students: [],
-        };
-    },
-    methods: {
-        getChildren(){
-            ParentService.getStudentsByParentId(this.parentId).then((response) => {
-                this.students = response.data;
-            });
-        }
-    },
-    created() {
-        this.getChildren();
-    }
-}
+  },
+  created() {
+    this.getChildren();
+  },
+};
 </script>
 
 <style>
-
 </style>
