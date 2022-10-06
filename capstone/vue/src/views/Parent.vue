aed99680a6b4b60386cf67a53d7ae92a511bae51
Jimmy Castillejo <jimcastillejo@gmail.com>
2022-10-06 12:52:28 -0500

    fixes

--- capstone/vue/src/views/Parent.vue
+++ capstone/vue/src/views/Parent.vue
@@ -53,22 +53,8 @@ export default {
   justify-content: space-evenly;
   align-content: center;
   text-align: center;
-<<<<<<< HEAD
 
   padding: 200px;
-=======
-  background: linear-gradient(
-    360deg,
-    white,
-    rgb(241, 197, 99),
-    rgb(253, 223, 158),
-    white
-  );
-  padding-bottom: 200px;
-  /* padding: 200px; */
-  padding-right: 400px;
-  padding-left:400px;
->>>>>>> 6dc6b99f8f63e718e9b975b5fac06ce26a9d6ea0
 }
 
 .leaderboard {
