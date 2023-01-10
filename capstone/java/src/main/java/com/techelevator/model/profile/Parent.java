4c333dbc6905a03140648261c8483467e0b2e6eb
Katie Kasberg <katiekasberg@gmail.com>
2023-01-09 20:26:10 -0500

    first commit

---
+++ capstone/java/src/main/java/com/techelevator/model/profile/Parent.java
@@ -0,0 +1,22 @@
+package com.techelevator.model.profile;
+
+public class Parent extends Profile{
+    private String phoneNumber;
+    private String address;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
